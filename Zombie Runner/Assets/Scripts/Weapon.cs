57d808812d3cc54f03241871cdc8f6fc2df0ffda
GoncaloRego <goncalorego30@gmail.com>
2021-11-30 11:04:24 +0000

    Player movement; Enemy following player;

---
+++ Zombie Runner/Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range);
+        Debug.Log(hit.transform.name);
+    }
+}
